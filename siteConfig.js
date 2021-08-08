d40afaf1da11c6aa44d127e3f7797e973d8a7145
Murali krishna Bellamkonda <github@paperbun.org>
2021-08-08 21:19:15 +0530

    first commit

---
+++ siteConfig.js
@@ -0,0 +1,30 @@
+module.exports = {
+    // Do not include a trailing slash!
+    siteUrl: `http://localhost/`,
+
+    // Enable infinite scroll (default: true)
+    infiniteScroll: true,
+
+    // Initial number fetched, scrolling lazy loads posts one by one
+    // If infinite scroll is disabled: maximum number of post shown per page
+    postsPerPage: 3,
+
+    // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Paperbun`,
+
+    // This allows an site description for meta data for pages.
+    siteDescriptionMeta: `With illuminating stories`,
+
+    // Used for App and Offline manifest e.g. Mobile Home Screen
+    shortTitle: `With illuminating stories`,
+    siteIcon: `favicon.png`,
+    backgroundColor: `#e9e9e9`,
+    themeColor: `#15171A`,
+
+    // Show more logs for debugging purposes (default: false)
+    verbose: false,
+
+    // Severity for verbose mode: (`info`, `warn`, `error`)
+    severity: `info`,
+
+}
